0c3703a2bdfdd2fd25e684d3e75e94613c469054
Kaung-Phyo-Wai <kaungphyowaidecember@gmail.com>
2020-12-04 03:32:28 +0630

    Project Complete

--- lib/screens/note_screen.dart
+++ lib/screens/note_screen.dart
@@ -1,11 +1,93 @@
 import 'package:flutter/material.dart';
 
+import '../model/note_model.dart';
+import 'package:intl/intl.dart';
+
 class NoteScreen extends StatefulWidget {
   @override
   _NoteScreenState createState() => _NoteScreenState();
 }
 
-class _NoteScreenState extends State<NoteScreen> {
+class _NoteScreenState extends State<NoteScreen>
+    with SingleTickerProviderStateMixin {
+  int _selectedCatergoryIndex = 0;
+  TabController _tabController;
+  final DateFormat _dateFormatter = DateFormat('dd MMM');
+  final DateFormat _timeFormatter = DateFormat('h:mm');
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      initialIndex: 0,
+      length: 3,
+      vsync: this,
+    );
+  }
+
+  Widget _buildCategoryCard(int index, String title, int content) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedCatergoryIndex = index;
+        });
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+        height: 240,
+        width: 175,
+        decoration: BoxDecoration(
+          color: _selectedCatergoryIndex == index
+              ? Color(0xFF417BFB)
+              : Color(0xfff5f7fb),
+          borderRadius: BorderRadius.circular(20.0),
+          boxShadow: [
+            _selectedCatergoryIndex == index
+                ? BoxShadow(
+                    color: Colors.black26,
+                    offset: Offset(0, 2),
+                    blurRadius: 10.0,
+                  )
+                : BoxShadow(
+                    color: Colors.transparent,
+                  ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                title,
+                style: TextStyle(
+                  color: _selectedCatergoryIndex == index
+                      ? Colors.white
+                      : Color(0xFFAFB4C6),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                content.toString(),
+                style: TextStyle(
+                  color: _selectedCatergoryIndex == index
+                      ? Colors.white
+                      : Colors.black,
+                  fontSize: 35.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,13 +107,188 @@ class _NoteScreenState extends State<NoteScreen> {
                   decoration: BoxDecoration(
                     image: DecorationImage(
                       image: AssetImage(
-                        'assets/images/user.png',
+                        'assets/user.png',
                       ),
                     ),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                SizedBox(width: 20.0),
+                Text(
+                  "Jenny Breaks",
+                  style: TextStyle(
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 )
               ],
             ),
+          ),
+          SizedBox(height: 40.0),
+          Container(
+            height: 280.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length + 1,
+              itemBuilder: (BuildContext context, int index) {
+                if (index == 0) {
+                  return SizedBox(
+                    width: 20.0,
+                  );
+                } else {
+                  return _buildCategoryCard(
+                    index - 1,
+                    categories.keys.toList()[index - 1],
+                    categories.values.toList()[index - 1],
+                  );
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0),
+            child: TabBar(
+              controller: _tabController,
+              labelColor: Colors.black,
+              unselectedLabelColor: Color(0xFFAFB4C6),
+              indicatorColor: Color(0xFF417BFB),
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorWeight: 4.0,
+              isScrollable: true,
+              tabs: [
+                Tab(
+                  child: Text(
+                    'Notes',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'Important',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'Performed',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Note(
+            timeFormatter: _timeFormatter,
+            dateFormatter: _dateFormatter,
+            index: 0,
+          ),
+          Note(
+            timeFormatter: _timeFormatter,
+            dateFormatter: _dateFormatter,
+            index: 1,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Note extends StatelessWidget {
+  const Note({
+    Key key,
+    @required DateFormat timeFormatter,
+    @required DateFormat dateFormatter,
+    @required this.index,
+  })  : _timeFormatter = timeFormatter,
+        _dateFormatter = dateFormatter,
+        super(key: key);
+
+  final DateFormat _timeFormatter;
+  final DateFormat _dateFormatter;
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(30.0),
+      padding: EdgeInsets.all(30.0),
+      decoration: BoxDecoration(
+        color: Color(0xFFF5F7FB),
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                notes[index].title,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                _timeFormatter.format(notes[index].date),
+                style: TextStyle(
+                  color: Color(0xFFAFB4C6),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          Text(
+            notes[index].content,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                _dateFormatter.format(notes[index].date),
+                style: TextStyle(
+                  color: Color(0xFFAFB4C6),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: Color(0xFF417BFB),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Icon(
+                  Icons.location_on,
+                  color: Colors.white,
+                ),
+              ),
+            ],
           ),
         ],
       ),
